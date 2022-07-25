a71ac4c1befb45c12a32293941faf1ff684ffbcc
Dhanashree <mone.dhanashree25@gmail.com>
2022-07-25 12:17:30 +0530

    Removed commented code

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -6,16 +6,6 @@ import "./App.css"
 function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState("")
-
-  // const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=Metric&appid=1f8f362a765c09af83e1be56628603d0`
-  
-  
-  // console.log(location)
- 
-  // const searchLocation = ()  =>{ 
-  //   setData({wind:"abc", main:"cde", weather:"jhh", name:"dcc"})
-  //   setLocation()
-  // }
   const [latitude, setlatitude] = useState(0);
   const [longitude, setylongitude] = useState(0);
 
@@ -24,21 +14,16 @@ function App() {
     setylongitude(position.coords.longitude);
   }
 
-  
-
   const weatherFetch = async () => {
     try{
-      // await window.navigator.geolocation.getCurrentPosition(savePositionToState)
-     
       const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=Metric&appid=1f8f362a765c09af83e1be56628603d0`) ;
       setData(res.data)
       console.log(res.data)
     }catch(err){
       console.log(err)
     }
-    // setLocation('')
   }
-  //9577ed9712a7b415338789f085bfb72f
+  
   const searchLocation = async() => {
     try{
       const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=Metric&appid=1f8f362a765c09af83e1be56628603d0`);
@@ -46,71 +31,30 @@ function App() {
     }catch(err){
       console.log(err)
     }
-    
   }
 
   const clickHandler = () => {
-    // setLocation(location)
     searchLocation()
     setLocation('')
   }
 
-
-  // const searchLocation = () =>{
-  //   axios.get(url).then(Response => {
-  //     setData(Response.data)
-  //     console.log(Response.data)
-  //   })
-  //   // {location && <Main data={data} location={location}/>}
-  //   setLocation('')
-  // }
-
-  // const searchLocation = () =>{
-  //   axios.get(url).then(Response => {
-  //     setData(Response.data)
-  //     console.log(Response.data)
-  //   })
-  //   // {location && <Main data={data} location={location}/>}
-  //   setLocation('')
-  // }
-
-  // useEffect(() => {
-  //   clickHandler()
-  // },[location])
-  
   useEffect(async() => {
-    // location? clickHandler() : weatherFetch();
-    // if(location !== ""){
-    //   searchLocation()
-    // }
-    // else{
-    //   weatherFetch();
-    // }
-  
     window.navigator.geolocation.getCurrentPosition(savePositionToState)
      weatherFetch()
   },[latitude && longitude])
 
-  
-  
-  
   return (
     <div className="App">
       <div className="search">
-        {/* <p>{"http://openweathermap.org/img/w/" + location + ".png"}</p> */}
       <input
       value = {location}
       onChange = {event => setLocation(event.target.value)}
-      // onKeyPress = {searchLocation}
       placeholder = "Enter Location"
       type="text"/>
-      
-      {/* {data.name && <button onClick={searchLocation} className="btn">Search</button>} */}
-
+     
       <button onClick={clickHandler} className="btn">Search</button>
 
       </div>
-      {/* {location  && <Main data={data} location={location}/>} */}
       <Main data={data} location={location}/>
     </div>
   );
--- weatherapp/src/Components/Main.js
+++ weatherapp/src/Components/Main.js
@@ -2,59 +2,45 @@ import React from 'react'
 import "./Main.css"
 
 function Main(props) {
-  // let location = props.location
-  // let name = props.data.name
   return (
     <div className="container">
-      
-      {/* {(props.data.name && props.location && props.data.name.LowerCase() === props.location.LowerCase()) ? 
-      <div> */}
       {props.data.main ?
-      <>
-        <div className="top">
-          <div className="location">
-            {/* <p>London</p> */}
-            <p>{props.data.name}</p>
-          </div>
-          {props.data.weather[0]?
-          <>
-            <div className="icon">
-              {/* <p>⛅</p> */}
-              <img className='image' src ={"http://openweathermap.org/img/w/" + props.data.weather[0].icon + ".png"} />
+        <>
+          <div className="top">
+            <div className="location">
+              <p>{props.data.name}</p>
             </div>
-            <div className="desciption">
-              {/* <p>cloudy</p> */}
-              <p>{props.data.weather[0].main}</p>
+            {props.data.weather[0] ?
+              <>
+                <div className="icon">
+                  <img className='image' src={"http://openweathermap.org/img/w/" + props.data.weather[0].icon + ".png"} />
+                </div>
+                <div className="desciption">
+                  <p>{props.data.weather[0].main}</p>
+                </div>
+              </>
+              : null}
+            <div className="temp">
+              <h1>{props.data.main.temp.toFixed()}°C</h1>
             </div>
-          </>
-          :null}
-          <div className="temp">
-            {/* <h1>30°C</h1> */}
-             <h1>{props.data.main.temp.toFixed()}°C</h1>
           </div>
-        </div>
-        <div className="bottom">
-          <div className="feels">
-          {/* <p className='bold'>35°C</p> */}
-            <p className='bold'>{props.data.main.feels_like.toFixed()}°C</p>
-            <p>Feels Like</p>
+          <div className="bottom">
+            <div className="feels">
+              <p className='bold'>{props.data.main.feels_like.toFixed()}°C</p>
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              <p className='bold'>{props.data.main.humidity}%</p>
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {props.data.wind ? <p className='bold'>{props.data.wind.speed}MPH</p> : null}
+              <p>Wind Speed</p>
+            </div>
           </div>
-          <div className="humidity">
-          {/* <p className='bold'>10%</p> */}
-            <p className='bold'>{props.data.main.humidity}%</p>
-            <p>Humidity</p>
-          </div>
-          <div className="wind">
-            {props.data.wind? <p className='bold'>{props.data.wind.speed}MPH</p>: null}
-            {/* {props.data.wind? <p className='bold'>1.3MPH</p>: null} */}
-            <p>Wind Speed</p>
-          </div>
-        </div>
         </>
-        :null}
-       </div>
-    //     :<p>Aur karna hai kam </p>}
-    // </div>
+        : null}
+    </div>
   )
 }
 
